146cc0a4df15cb429fe96ad0cd9ec159e7543ad8
JJohnR08 <rassijos000@rocori.k12.mn.us>
2026-03-16 16:36:26 +0000

    Change

---
+++ TheYear.java
@@ -0,0 +1,7 @@
+public class TheYear {
+    public static void main(String[] args) {
+        int year = 2026; 
+        System.out.println("Welcome to Java!");
+        System.out.println("The year is. " + year);
+    }
+}
---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld {
+    public static void main(String[] args) {
+        String name = "Joseph";
+        String FavSub = "";
+        String school = "ROCORI high school";
+        String favoriteNumber = "6";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(" School: " + school + ", favorite number: " + favoriteNumber);
+    }
+}
